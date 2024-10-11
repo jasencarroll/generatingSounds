8903bbbe21531fe7d0c4c80ced01e3c2884c349f
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-10-11 05:52:45 -0400

    getting started

---
+++ script.js
@@ -0,0 +1,22 @@
+let play = document.querySelector("#play");
+let playing = document.querySelector("#playing");
+play.addEventListener("click", () => {
+    // Hide this button
+    play.style = "display: none";
+    playing.style = " ";
+
+    let audioCtx = new AudioContext();
+
+    let oscNode = audioCtx.createOscillator();
+    oscNode.frequency.value = 440;
+
+    let gainNode = audioCtx.createGain();
+    gainNode.gain.value = 0.5;
+
+    oscNode.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    oscNode.start(audioCtx.currentTime);
+    oscNode.stop(audioCtx.currentTime + 2);
+
+});
